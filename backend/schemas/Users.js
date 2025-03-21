775f2453a4a4eb9947b442dcb0d60408e2bbfff2
jamesharris1307 <jamesharris1307@outlook.com>
2025-03-21 12:04:07 +0000

    fixed schema Users

--- backend/schemas/Users.js
+++ backend/schemas/Users.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const UserShema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId , auto:true},
     email:{ type: String, required: true},
     password:{type: String, required: true},
