f5af2adf7d19f88aeed46c42d6f389d34b285846
jamesharris1307 <jamesharris1307@outlook.com>
2025-03-22 16:44:21 +0000

    settings css html js logic added

--- FrontEnd/js/settings.js
+++ FrontEnd/js/settings.js
@@ -16,18 +16,19 @@ function displaySettingsContent() {
                 <div id="settingsContentDiv">
                     <div id="titleContainer">
                         <h1 id="settingsTitleText" >Settings</h1>
+                        <p>Click the links below</p>
                     </div>
                     <div id="fontSizeContainer">
                         <a href=""><h1>Font Size</h1></a>
-                        <p>Small, Medium, Large</p>
+                        <p id="smallFontSizeText" >Small, Medium, Large</p>
                     </div>
                     <div id="versionContainer">
                         <a href=""><h1>About the App</h1></a>
-                        <p>Version Info, Credits</p>
+                        <p id="smallAboutAppText" >Version Info, Credits</p>
                     </div>
                     <div id="feedbackContainer">
-                        <a href=""><h1>Feedback & Support</h1></a>
-                        <p>Help Improve the App</p>
+                        <a href="feedback.html"><h1>Feedback & Support</h1></a>
+                        <p id="smallFeedbackText" >Help Improve the App</p>
                     </div>
                 </div>
     `;
