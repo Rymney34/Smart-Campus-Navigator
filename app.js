028b4aa1c77b8b2932ee0f6adac93770cb80683e
jamesharris1307 <jamesharris1307@outlook.com>
2025-03-23 16:19:12 +0000

    BackEnd Folder Added

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express'); 
 const mongoose = require('mongoose');
 const cors = require("cors");
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./BackEnd/routes/userRoutes");
 const path = require('path') // Require path module (Handle File Paths) Used when launching the website FrontEnd 
-require('./dbConnect'); // Connect to MongoDB Databases
+require('./BackEnd/dbConnect'); // Connect to MongoDB Databases
 
 const app = express();
 const port = 3000;
@@ -14,7 +14,7 @@ app.use(cors()); // Enable CORS
 
 // Launch Website Front End
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'FrontEnd','Views', 'login.html')); // Serve login.html
+  res.sendFile(path.join(__dirname, 'FrontEnd', 'Views', 'login.html')); // Serve login.html
 });
 
 // API Routes
