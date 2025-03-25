8f356a60c65ebb87b9e7af476201e5bac3e0b297
jamesharris1307 <jamesharris1307@outlook.com>
2025-03-25 17:03:25 +0000

    init

--- BackEnd/schemas/images.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const imagesSchema = new mongoose.Schema({
-    _id: { type: mongoose.Schema.Types.ObjectId,auto:true  },
-   image: { type: Buffer },
-});
-
-module.exports = mongoose.model("images", imagesSchema)
