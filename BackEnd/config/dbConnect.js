359baf75bc9115074ba6ba0f8935704119c65aa4
Lucasarl <90321541+Lucasarl@users.noreply.github.com>
2025-03-27 15:38:55 +0100

    .env uris for local docker development and remote atlas development

--- BackEnd/config/dbConnect.js
+++ BackEnd/config/dbConnect.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Default connection (smart_campus_navigator)
 // async function connectDB() {
@@ -11,13 +12,13 @@ const mongoose = require('mongoose');
 // }
 
 mongoose.connect(
-  'mongodb+srv://first_db_userT:Gazoz_228@campus-navigator.qe53f.mongodb.net/smart_campus_navigator?retryWrites=true&w=majority')
+  process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB (smart_campus_navigator)'))
   .catch(err => console.error('Connection error (smart_campus_navigator):', err));
 
 // Secondary connection (user_details_db)
 const usersConnection = mongoose.createConnection(
-  'mongodb+srv://first_db_userT:Gazoz_228@campus-navigator.qe53f.mongodb.net/user_details_db?retryWrites=true&w=majority'
+  process.env.MONGO_URI
 );
 
 usersConnection.on('connected', () => {
