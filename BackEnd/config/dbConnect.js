f98a43ee801e11f922f9dd2c15a8f392c83c98fa
jamesharris1307 <jamesharris1307@outlook.com>
2025-04-12 11:44:43 +0100

    fix - .env file is now being used as intended instead of direct connection string in database connection file (dbConnect.js).

--- BackEnd/config/dbConnect.js
+++ BackEnd/config/dbConnect.js
@@ -2,16 +2,13 @@ const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
-
-
-
 // Default connection (smart_campus_navigator)
-mongoose.connect('mongodb+srv://first_db_userT:Gazoz_228@campus-navigator.qe53f.mongodb.net/smart_campus_navigator?retryWrites=true&w=majority)//process.env.MONGO_URI_MAIN')
+mongoose.connect(process.env.MONGODB_URI_CAMPUSDB,)
   .then(() => console.log('Connected to MongoDB (smart_campus_navigator)'))
   .catch(err => console.error('Connection error (smart_campus_navigator):', err));
 
 // Secondary connection (user_details_db)
-const usersConnection = mongoose.mongoose.createConnection('mongodb+srv://first_db_userT:Gazoz_228@campus-navigator.qe53f.mongodb.net/user_details_db?retryWrites=true&w=majority');//process.env.MONGO_URI_USERS);
+const usersConnection = mongoose.mongoose.createConnection(process.env.MONGODB_URI_USERDB,);
 
 usersConnection.on('connected', () => {
   console.log('Connected to MongoDB (user_details_db)');
